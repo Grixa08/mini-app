3f057a19ef0b783ad85ba8f9c4a5b2086c306a44
Grixa08 <braznik-03@list.ru>
2025-03-04 00:04:53 +0700

    fix: улучшение

--- src/components/ActivitiesScreen.tsx
+++ src/components/ActivitiesScreen.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Worker } from '@react-pdf-viewer/core';
+import { Viewer } from '@react-pdf-viewer/core';
+import * as pdfjs from 'pdfjs-dist';
+import 'pdfjs-dist/build/pdf.worker.entry'; // Для Webpack/Vite
+
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
@@ -9,8 +12,9 @@ interface ActivitiesScreenProps {
   onBack: () => void;
 }
 
-const ActivitiesScreen: React.FC<ActivitiesScreenProps> = ({ onBack }) => {
+pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
+const ActivitiesScreen: React.FC<ActivitiesScreenProps> = ({ onBack }) => {
   return (
     <div>
       <div className='btn-exit'>
@@ -19,13 +23,9 @@ const ActivitiesScreen: React.FC<ActivitiesScreenProps> = ({ onBack }) => {
         </button>
         <h2>Получить баллы</h2>
       </div>
-      
+
       <div className="pdf-container">
-        <Worker workerUrl={`https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.worker.min.js`}>
-          <div className="pdf-viewer">
-            <iframe src={pdfFile} title="PDF Viewer" width="100%" height="700px"></iframe>
-          </div>
-        </Worker>
+        <Viewer fileUrl={pdfFile} />
       </div>
     </div>
   );
